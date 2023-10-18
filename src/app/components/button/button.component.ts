a76bb53fcde8325c76da2598364df4853105e418
unknown <camizupa.absti@gmail.com>
2023-10-18 19:09:30 -0300

    first commit

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'button-component',
+    templateUrl: './button.component.html',
+})
+export class ButtonComponent {
+    @Input() icon?: string ;
+}
