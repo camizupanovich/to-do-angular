6ff517b0fbf6c2395ac3e356b6d0bcf26fb8a345
unknown <camizupa.absti@gmail.com>
2023-10-19 09:33:01 -0300

    services

---
+++ src/app/services/to-do.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn: 'root'
+})
+export class PreliquidacionesService {
+    //Nueva injeccion de angular 16
+    private http = inject(HttpClient)
+
+    //actividades en memoria
+    public activities = [{
+        activityId: 3,
+        title: "Desayuno",
+        type: "FOOD",
+        startDate: null,
+        endDate: null,
+        status: null,
+    },
+    {
+        activityId: 1,
+        title: "Subida al cerro catedral",
+        type: "ACTIVITY",
+        startDate: "2022-01-22 01:30:00",
+        endDate: "2022-01-22 23:30:00",
+        status: "IN_PROGRESS",
+    },
+    {
+        activityId: 2,
+        title: "Fiesta de espuma",
+        type: "PARTY",
+        startDate: "2022-01-22 01:30:00",
+        endDate: "2022-01-22 23:30:00",
+        status: "DONE",
+    }];
+
+    //tipos de actividad en memoria
+    private types = ['FOOD', 'PARTY', 'ACTIVITY']
+
+    public getActivities() {
+        return this.activities
+    }
+
+    public postActivity(body: any) {
+        body['actividyId'] = this.activities.length + 1
+        this.activities.push(body);
+        return this.activities
+    }
+
+    public putActivity(body: any) {
+        const index = this.activities.findIndex(a => a.activityId === body.activityId);
+        this.activities[index] = body;
+        return this.activities
+    }
+    public getTypes() {
+        return this.types;
+    }
+}
